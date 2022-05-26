563f750d57de2b69ccef41331654c96f2abe4364
hritikpatidar <hritikpatidar1810@gmail.com>
2022-05-26 20:38:55 +0530

    latest version update

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import axios from 'axios';
 function App() {
   // 1. state/hook variable
   const [data1, setData1] = useState([])
-  
-  
+
+
   useEffect(() => {
     myCall()
   }, [])
 
   // 2. function defination
-  let myCall = async()=>{
+  let myCall = async () => {
     try {
-      const res =await axios.get('https://staging-api.dahmakan.com/test/orders?sort[0]=order_id&sort[1]=paid_withdesc')
+      const res = await axios.get('https://staging-api.dahmakan.com/test/orders?sort[0]=order_id&sort[1]=paid_withdesc')
       console.log(res.data.orders);
       setData1(res.data.orders)
     } catch (error) {
@@ -22,37 +22,38 @@ function App() {
     }
   }
   // 3. return statement
-  console.log("data.orders",data1);
+  console.log("data.orders", data1);
   return (
-      <table>
-        <thead>
-          <tr>
-            <th>order_id</th>
-            <th>arrives_at_utc</th>
-            <th>paid_with</th>
-            <th>total_paid</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            data1.length &&
-            data1.map((cv, index, arr) => {
-              console.log("cv",cv.order_id);
-              const unixTime = 	cv.arrives_at_utc	;
-              const date = new Date(unixTime);
-              const time =date.toLocaleDateString("en-US");
-              return (
-                <tr key={index}>
-                  <td>{cv.order_id}</td>
-                  <td>{time}</td>
-                  <td>{cv.paid_with}</td>
-                  <td>{cv.total_paid}</td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
+    <table class="table table-dark table-striped">
+      <thead>
+        <tr>
+          <th scope="col">order_id</th>
+          <th scope="col">arrives_at_utc</th>
+          <th scope="col">paid_with</th>
+          <th scope="col">total_paid</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          data1.length &&
+          data1.map((cv, index, arr) => {
+            console.log("cv",cv.order_id);
+            const unixTime = 	cv.arrives_at_utc	;
+            const date = new Date(unixTime);
+            const time =date.toLocaleDateString("en-US");
+            return (
+              <tr key={index}>
+                <td>{cv.order_id}</td>
+                <td>{time}</td>
+                <td>{cv.paid_with}</td>
+                <td>{cv.total_paid}</td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </table>
+      
   )
 }
 
